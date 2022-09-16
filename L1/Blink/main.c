23179ae7a914878acc1fec354a381ca5e61b4f72
vnu <vnullo@gmail.com>
2022-09-16 12:42:50 +0300

    Code fixed, report fixed

--- L1/Blink/main.c
+++ L1/Blink/main.c
@@ -1,5 +1,5 @@
 #include <msp430.h>
-#define WAIT_CYCLES 100000 // magic number. Чтобы перестало им быть , нужно узнать MCLK
+#define WAIT_CYCLES 100000 // magic number. „тобы перестало им быть , нужно узнать MCLK
 // https://stackoverflow.com/questions/34258254/msp430-delay-cycles-is-slow-mclk-might-be-slow
 volatile long int i = 0;
 volatile char bState = 0;
@@ -17,40 +17,63 @@ void main(void)
 
     //led 1 on p1.0
     //out
-    P1DIR |= BIT0;
-    //en
-    P1OUT |= BIT0;
+    P1DIR |= BIT1;
+//    //en
+//    P1OUT |= BIT0;
+    //off
+      P1OUT &= ~BIT1;
 
     //led 2 on 8.1
     //out
-    P8DIR |=BIT1;
-    //en
-    P8OUT |=BIT1;
+    P1DIR |=BIT5;
+//    //en
+//    P8OUT |=BIT1;
+    //off
+     P1OUT &=~BIT5;
+//    while(1)
+//    {
+//        for (i=0; i<WAIT_CYCLES; i++);
+//        P1OUT^=BIT0;
+//
+//        //bState = !(P1IN & BIT7);
+//        for (i=0; i<WAIT_CYCLES; i++){
+//            bState = !(P1IN & BIT7);
+//            if (!bState){
+//                P1OUT&=~BIT0;
+//                P8OUT&=~BIT1;
+//            }
+//            else {
+//                P8OUT|=BIT1;
+//            }
+//            ;
+//        }
+//        P8OUT^=BIT1;
+////        for (i=0; i<WAIT_CYCLES; i++){
+////            bState = !(P1IN & BIT7);
+////            if (!bState){
+////                P1OUT&=~BIT0;
+////                P8OUT&=~BIT1;
+////                        }
+////            else {
+////                P8OUT|=BIT1;
+////            }
+////        }
+//
+//        //P8OUT^=BIT1;
+//    }
     while(1)
-    {
-        //bState = !(P1IN & BIT7);
-        for (i=0; i<WAIT_CYCLES; i++){
-            bState = !(P1IN & BIT7);
-            if (!bState){
-                P1OUT&=~BIT0;
-                P8OUT&=~BIT1;
-            }
-            else {
-                P1OUT|=BIT0;
-            }
-            ;
-        }
-        P1OUT^=BIT0;
-        for (i=0; i<WAIT_CYCLES; i++){
-            bState = !(P1IN & BIT7);
-            if (!bState){
-                            P1OUT&=~BIT0;
-                            P8OUT&=~BIT1;
-                        }
-            else {
-                P8OUT|=BIT1;
-            }
-        }
-        P8OUT^=BIT1;
-    }
+       {
+           while (!(bState = !(P1IN & BIT7))){
+               P1OUT &= ~BIT1;
+               P1OUT &=~BIT5;
+
+               ;
+           }
+           for (i=0; i<WAIT_CYCLES; i++){
+           }
+           P1OUT^=BIT1;
+           for (i=0; i<WAIT_CYCLES; i++);
+           P1OUT^=BIT5;
+
+       }
 }
